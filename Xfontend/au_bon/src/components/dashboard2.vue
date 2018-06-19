698bae1322978f56c014ac8570fcbbf8bb3ca99c
Abhishek Jha <j7.abhi@gmail.com>
2018-06-19 13:45:46 +0530

    dashboard_frontend

---
+++ Xfontend/au_bon/src/components/dashboard2.vue
@@ -0,0 +1,17 @@
+<template>
+    <navbar></navbar>
+</template>
+
+<script>
+import Nav from './dashboard_components/navbar.vue';
+export default {
+components:{
+    'navbar': Nav,
+}
+}
+</script>
+
+
+<style>
+
+</style>
--- Xfontend/au_bon/src/main.js
+++ Xfontend/au_bon/src/main.js
@@ -4,10 +4,12 @@ import VueRouter from 'vue-router';
 
 import Login from './components/login.vue';
 import Dashboard from './components/dashboard.vue';
+import New_dashboard from './components/dashboard2.vue';
 Vue.use(VueRouter);
 const routes = [
   { path: '', component: Login},
-  { path: '/dashboard', component: Dashboard}
+  { path: '/dashboard_old', component: Dashboard},
+  { path: '/dashboard', component: New_dashboard},
 ];
 
 const router = new VueRouter({

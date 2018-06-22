9d03e9df37b6c0c219f739cbcd2b8902cff47640
Abhishek Jha <j7.abhi@gmail.com>
2018-06-22 20:49:27 +0530

    vue app moved to laravel

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
 Route::post('/login', 'loginController@login');
 Route::get('dashboard', function () {

221ab770138952d04f88e22ec042c66113b8d8b2
Abhishek Jha <j7.abhi@gmail.com>
2018-06-22 20:37:40 +0530

    dashboard linked to after login

--- Xfontend/au_bon/src/components/main.vue
+++ Xfontend/au_bon/src/components/main.vue
@@ -1,23 +1,35 @@
 <template>
+
 <div id="main">
-    <div>
+     <div v-if="logged_in">
+        <app-dashboard></app-dashboard>
+
+    </div>
+    <div v-else >
         <app-login v-on:login-successful="loginSuccess"></app-login>
     </div>
+   
 </div>
     
 </template>
 
 <script>
 import Login from './login.vue';
+import Dashboard from './dashboard2.vue';
 export default {
+    data(){
+        return {
+            logged_in:false,
+
+        }
+    },
 components:{
-    'app-login':Login
+    'app-login':Login,
+    'app-dashboard':Dashboard
 },
 methods:{
     loginSuccess(fullname,role){
-        console.log('Hahahaha');
-        document.getElementById('main').innerHTML=role+'<h1>Welcome '+fullname+'</h1>';
-
+        this.logged_in=true;
     }
 }
 }

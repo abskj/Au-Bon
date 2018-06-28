5dac40af067525881c21d84591b903a5ff1761ae
Abhishek Jha <j7.abhi@gmail.com>
2018-06-28 15:05:31 +0530

    models: for bill transactions

---
+++ app/steward.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class steward extends Model
+{
+    //
+    protected $fillable=[
+        'name',
+        'branch_id',
+        'mobile',
+    ];
+}

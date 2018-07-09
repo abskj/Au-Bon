202aafbf5ad82910717430578ce4acbcc4aa5ad4
akram zaki <akram102zaki@gmail.com>
2018-07-09 11:50:38 +0530

    grdc

--- Xfontend/au_bon/src/components/dashboard_components/createStaff.vue
+++ Xfontend/au_bon/src/components/dashboard_components/createStaff.vue
@@ -1,14 +1,100 @@
 <template>
-    <h2>Create Staff</h2>
+    <div>
+        <div class="row">
+            <form @submit.prevent='Onsubmitted' class="col s12">
+              <div class="row">
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">account_circle</i>
+                  <input id="name" type="text" class="validate" v-model="name">
+                  <label for="icon_prefix">user name</label>
+                </div>
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">phone</i>
+                  <input id="mobile" type="number" class="validate" v-model="mobile">
+                  <label for="icon_telephone">Telephone</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">lock</i>
+                  <input id="PASSWORD" type="password" class="validate" v-model="password">
+                  <label for="PASSWORD">password</label>
+                </div>
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">account_circle</i>
+                  <input id="userfullname" type="text" class="validate" v-model="fullname">
+                  <label for="userfullname">Full name</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">location_on</i>
+                  <input id="address" type="text" class="validate" v-model="address">
+                  <label for="address">address</label>
+                </div>
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">account_circle</i>
+                  <input id="voterid" type="text" class="validate" v-model="voterid">
+                  <label for="voterid">voter id</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s6">
+                  <i class="material-icons prefix">account_circle</i>
+                  <input id="adahar_no" type="text" class="validate" v-model="adahar">
+                  <label for="adahar_no">adahar no</label>
+                </div>
+              </div>
+              <div>
+                  <button class="btn" type="submit">submit</button>
+              </div>
+        </form>
+  </div>
+
+    </div>
 </template>
 
 <script>
+import Axios from 'axios';
 export default {
+    data(){
+        return{
+            name:'',
+            password:'',
+            adahar:'',
+            fullname:'',
+            address:'',
+            mobile:'',
+            voterid:''
+        }
+    },
  props: {
         user: {
             type: Array
         }
     },
+    methods:{
+        Onsubmitted(){
+            Axios.post("http://127.0.0.1:8000/api/staff",
+            {
+            'user_name':this.name,
+            'password':this.password,
+            'aadhar_no':this.adahar,
+            'user_fname':this.fullname,
+            'address':this.address,
+            'admin_name':this.user[0]['user_name'],
+            'restro_id':this.user[0]['restro_id'],
+            'mobile':this.mobile,
+            'voter_id':this.voterid,
+            'branch_id':this.user[0]['branch_id']
+            })
+            .then(function(response){
+                console.log(response);
+            }).catch(function(error){
+                console.log(error);
+            });
+        }
+    }
 }
 </script>
 

1e9d6cb8126bb5da94a938b5709795706b410079
Abhishek Jha <j7.abhi@gmail.com>
2018-08-05 17:37:11 +0530

    backend: query support for history

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,16 +3,16 @@ namespace App\Http\Controllers;
 use App\bill_transaction;
 use App\steward;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 class TransactionController extends Controller{
     /*
     type index:(divisible by)
-    17   all
+    1   all
     2   contains from date
     3   contains to date
     5   customer number
-    7   food category
-    11  food item   
+    \
     */
     
     public function getList(Request $r){
@@ -20,9 +20,26 @@ class TransactionController extends Controller{
             'username' => 'required',
             'branch_id' => 'required',
             'type' => 'required',
+            
         ]);
         
-        $trans = bill_transaction::paginate(15);
+        $trans = DB::table('bill_transaction');
+        $type=$r->input('type');
+        if($type%5==0){
+            $trans=$trans->where('cust_id',$r->input('cust_id'));
+        }
+        if($type%2==0){
+            $fromDate=date($r->input('fromDate'));
+            $trans=$trans->whereDate('created_at','>=',$fromDate);
+        }
+        if($type%3==0){
+            $toDate=date($r->input('toDate'));
+            $trans=$trans->whereDate('created_at','<=',$toDate);
+        }
+
+        $trans=$trans->paginate(15);
+
+
         return response()->json([
             'data'=>$trans,
 
